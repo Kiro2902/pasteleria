ddd2fd0b89d1cba5b013d0a662337541c10eecb4
marcelo <kiro2904@gmail.com>
2026-04-08 21:44:44 -0500

    Inicio de Proyecto

---
+++ PasteleriaWebApp/PasteleriaWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PasteleriaWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

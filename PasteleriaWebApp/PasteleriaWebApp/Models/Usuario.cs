ce9ff7a62fb709b6635210e0c246972867454a12
Anderson <anderson140815@gmail.com>
2026-04-09 16:31:38 -0500

    feat: implementacion de modelos Usuario y Rol

---
+++ PasteleriaWebApp/PasteleriaWebApp/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace PasteleriaWebApp.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        public int IdRol { get; set; }
+
+        // Propiedad extra para guardar el nombre del rol (Admin o Empleado)
+        public string NombreRol { get; set; }
+    }
+}
